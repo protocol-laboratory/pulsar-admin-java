8b62f9adfedf878e6a89bf830fdb1aa6b4997dda
ZhangJian He <shoothzj@gmail.com>
2023-03-04 18:17:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# pulsar-admin-java
+This is a simple alternative to pulsar-admin built using the built-in HTTP client of the JDK. It minimizes project dependencies and requires a minimum of JDK 17.
+
+Features:
+- Basic functionality for managing Pulsar clusters 
+- Built on top of the built-in HTTP client of the JDK 
+- Minimizes project dependencies to provide a lightweight solution 
+- Requires a minimum of JDK 17
---
+++ src/main/java/io/github/protocol/pulsar/PulsarAdminBuilderImpl.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.github.protocol.pulsar;
+
+public class PulsarAdminBuilderImpl implements PulsarAdminBuilder {
+    private final Configuration conf;
+
+    public PulsarAdminBuilderImpl() {
+        this.conf = new Configuration();
+    }
+
+    @Override
+    public PulsarAdmin build() {
+        return new PulsarAdminImpl(conf);
+    }
+
+    @Override
+    public PulsarAdminBuilder host(String host) {
+        this.conf.setHost(host);
+        return this;
+    }
+
+    @Override
+    public PulsarAdminBuilder port(int port) {
+        this.conf.setPort(port);
+        return this;
+    }
+}
