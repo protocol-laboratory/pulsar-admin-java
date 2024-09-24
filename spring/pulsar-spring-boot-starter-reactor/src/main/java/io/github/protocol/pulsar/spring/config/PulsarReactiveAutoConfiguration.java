84a0f3d3bff8ce6b3bc2bc551e48e27b0599574c
ZhangJian He <shoothzj@gmail.com>
2024-09-24 15:03:51 +0800

    test: adapt test coverage
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

---
+++ spring/pulsar-spring-boot-starter-reactor/src/main/java/io/github/protocol/pulsar/spring/config/PulsarReactiveAutoConfiguration.java
@@ -0,0 +1,7 @@
+package io.github.protocol.pulsar.spring.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+@AutoConfiguration
+public class PulsarReactiveAutoConfiguration {
+}
